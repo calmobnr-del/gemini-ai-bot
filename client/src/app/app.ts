e42773e820227460fcd84a3eb48712adf6ccacf6
calm <calm.obnr@gmail.com>
2025-08-19 16:22:03 +0300

    chore: remove unused App module components and add Counter Store implementation with @ngrx signals

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,9 +3,12 @@ import { RouterModule } from '@angular/router';
 import { Button, Header } from '@gemini-ai-bot/ui';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { CounterServiceStore } from './store/counter.store';
+
 
 @Component({
   imports: [RouterModule, Button, Header, Header],
+  providers:[ ],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
@@ -14,6 +17,7 @@ export class App {
   protected title = 'gemini-ai-bot';
   private http = inject(HttpClient);
 
+  readonly store = inject(CounterServiceStore);
   private apiUrlChat = environment.apiUrlChat;
 
   constructor() {
